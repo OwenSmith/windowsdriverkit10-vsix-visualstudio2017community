560ebf3c3e79304656e8ee3a091793de2436fb7e
Owen Smith <owen.smith@citrix.com>
2018-08-13 15:22:55 +0100

    Add nuspec that executes the VSIX installer for the WDK10
    Signed-off-by: Owen Smith <owen.smith@citrix.com>

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,7 @@
+﻿
+$ErrorActionPreference = 'Stop'; # stop on all errors
+$vsixInstaller = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\VSIXInstaller.exe"
+$vsixArgs = "/quiet"
+$kitVsixFile = "C:\Program Files (x86)\Windows Kits\10\Vsix\WDK.vsix"
+
+& $vsixInstaller $vsixArgs "$kitVsixFile"
